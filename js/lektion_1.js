583817fc17c49759212e87147d26d20e50e7de11
maylova93 <142877128+maylova93@users.noreply.github.com>
2024-06-11 09:15:48 +0200

    opgaverMakka

--- js/lektion_1.js
+++ js/lektion_1.js
@@ -17,3 +17,39 @@ function fullName(firstName, lastName) {
 
 // Function Calls
 fullName("Ukendt", "Person");
+ function toggleClass(elementId, className){
+  let element = document.getElementById(elementId)
+  element.classList.toggle(className);
+ }
+
+
+ // Opgave 3
+function toggleColor(elementId, color) {
+  let element = document.getElementById(elementId);
+  if (element.style.backgroundColor === color) {
+    element.style.backgroundColor = '';
+    alert('Color removed');
+  } else {
+    element.style.backgroundColor = color;
+    alert('Color changed to ' + color);
+  }
+}
+
+//opgave 4
+let clickCount = 0;
+function countClicks(elementId){
+  clickCount++;
+  document.getElementById(elementId).innerHTML = `<p>Clicks: ${clickCount}</p>`;
+}
+
+// Opgave 5
+function wordLength(word) {
+  const length = word.length;
+  document.getElementById('L1_5').innerHTML += `<p>Word: ${word}, Length: ${length}</p>`;
+}
+
+// Opgave 6
+function removeVowels(word) {
+  const result = word.replace(/[aeiouAEIOU]/g, '');
+  document.getElementById('L1_6').innerHTML += `<p>Word without vowels: ${result}</p>`;
+}
--- js/lektion_2.js
+++ js/lektion_2.js
@@ -3,3 +3,39 @@ let L2_2 = document.querySelector("#L2_2");
 let L2_3 = document.querySelector("#L2_3");
 let L2_4 = document.querySelector("#L2_4");
 let L2_5 = document.querySelector("#L2_5");
+
+//Opgave 1
+function chekType(value){
+    const type = typeof value;
+    L2_1.innerHTML += `<p>${value} er af typen: ${type}</p>`;
+}
+
+// Opgave 2
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = regex.test(email);
+    L2_2.innerHTML += `<p>Email is ${isValid ? 'valid' : 'invalid'}</p>`;
+  }
+  // Opgave 3
+function checkStringLength(str) {
+    const isValid = str.length >= 2;
+    L2_3.innerHTML += `<p>String is ${isValid ? 'valid' : 'invalid'}</p>`;
+  }
+
+  // Opgave 4
+function getRandomNumber(min, max) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    L2_4.innerHTML += `<p>Vores tilfældige tal mellem ${min} og ${max} er: ${randomNumber}</p>`;
+  }
+  // Opgave 5
+function getRandomRGBColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const rgbColor = `rgb(${r}, ${g}, ${b})`;
+    const element = document.createElement('div');
+    element.style.backgroundColor = rgbColor;
+    element.style.width = '100px';
+    element.style.height = '100px';
+    L2_5.appendChild(element);
+  }
